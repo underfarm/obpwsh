e1f96bd580a03e38dd906f6df02580391e23a57e
Ulrik B. Farmen <ulrik.bruun.farmen@gmail.com>
2020-05-25 00:14:51 +0200

    works better now

--- obpwsh.el
+++ obpwsh.el
@@ -19,30 +19,26 @@
   ;; Round up the stuff we need
   (let* ((expanded-body (org-babel-expand-body:pwsh body params)))
     (ob-pwsh-initiate-term)
-    ;; Build script in temporary file
     (exec-pwsh-with-term expanded-body)))
 
 (defun exec-pwsh-with-term (toSend)
   (with-current-buffer "*terminal*"
+    (clear-term-buffer)
     (let* ((powershell-prompt-regex "PS [^#$%>]+>.+#!#")
            (prevpoint (point))
-           (sendString (concat toSend " #!#"))
+           (sendString toSend)
            (proc (get-buffer-process (get-buffer "*terminal*"))))
-      (term-send-string proc sendString)
-      (sleep-for 0 5) ;; This is needed because..wtf? race-condition?
+      (term-send-string proc (concat sendString "\n #!#"))
 
       ;; Wait until the prompt have returned with #!# at the end.
-      (if (eq nil (re-search-forward powershell-prompt-regex nil t))
-          (sleep-for 1))
-
-      ;; I dont want to have our 'end of code' displayed, eg '#!#'.
-      (goto-char (point-max))
-      (previous-line)
-      (goto-char (point-at-eol))
+      ;;
+      (while
+          (unless (re-search-backward powershell-prompt-regex nil t)
+            (sit-for 2)))
 
       (setq termoutput (buffer-substring-no-properties prevpoint (point) ))
+      (clear-term-buffer) ;; This is needed because..wtf? race-condition?
       (term-send-string proc " \n"))
-    (clear-term-buffer) ;; This is needed because..wtf? race-condition?
     termoutput))
 
 
